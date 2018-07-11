1c3c135f85a792f2398ac9744e34cd716a07dfc6
Shiva Kanth <shivakanth.sujit@gmail.com>
2018-07-11 22:26:23 +0530

    Database Implemented

--- app/src/main/java/io/github/shivakanthsujit/procrastinatorplus/CustomListAdapter.java
+++ app/src/main/java/io/github/shivakanthsujit/procrastinatorplus/CustomListAdapter.java
@@ -1,7 +1,10 @@
 package io.github.shivakanthsujit.procrastinatorplus;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,6 +20,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+
+import static android.content.Context.MODE_PRIVATE;
 
 public class CustomListAdapter extends ArrayAdapter {
     private final Activity context;
@@ -57,8 +62,9 @@ public class CustomListAdapter extends ArrayAdapter {
             holder.tdH = row.findViewById(R.id.head);
             holder.tdS = row.findViewById(R.id.sub);
             holder.tdC = row.findViewById(R.id.check);
-            holder.con = row.findViewById(R.id.confirm);
+
             holder.del = row.findViewById(R.id.del);
+            holder.tdL = row.findViewById(R.id.lo);
             LinearLayout li = row.findViewById(R.id.lo);
 
             holder.tdH.setText(td.get(position).head);
@@ -76,8 +82,10 @@ public class CustomListAdapter extends ArrayAdapter {
         {
             holder = (tdHolder) row.getTag();
         }
+
         holder.tdH.setOnClickListener(new View.OnClickListener() {
             private int pos = position;
+
             @Override
             public void onClick(View v) {
                 TextView t = (TextView)v;
@@ -86,6 +94,7 @@ public class CustomListAdapter extends ArrayAdapter {
                     t.setEnabled(true);
                     t.requestFocus();
 
+
                 }
             }
         });
@@ -93,23 +102,26 @@ public class CustomListAdapter extends ArrayAdapter {
             private int pos = position;
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
-                TextView t = (TextView)v;
-                String tt = t.getText().toString();
-                td.get(pos).head = tt;
-                //Toast.makeText(context,tt , Toast.LENGTH_SHORT).show();
-            }
+                if(hasFocus==false && td.size()!=0)
+                {
+
+                    TextView t = (TextView)v;
+                    String tt = t.getText().toString();
+                    td.get(pos).head = tt;
+                    storeDB(td);
+                }}
         });
         holder.tdS.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             private int pos = position;
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
-                if(hasFocus==false)
+                if(hasFocus==false && td.size()!=0)
                 {
 
                 TextView t = (TextView)v;
                 String tt = t.getText().toString();
                 td.get(pos).sub = tt;
-                //Toast.makeText(context,tt , Toast.LENGTH_SHORT).show();
+                storeDB(td);
             }}
         });
         holder.tdS.setOnClickListener(new View.OnClickListener() {
@@ -121,6 +133,7 @@ public class CustomListAdapter extends ArrayAdapter {
                     t.setSelected(false);
                     t.setEnabled(true);
                     t.requestFocus();
+
 
                 }
             }
@@ -137,12 +150,17 @@ public class CustomListAdapter extends ArrayAdapter {
                 {
                     LinearLayout li = row.findViewById(R.id.lo);
                     Toast.makeText(context,"Good job." , Toast.LENGTH_SHORT).show();
-                    li.setBackgroundColor(Color.GRAY);
+                    td.get(pos).done = 1;
+                    storeDB(td);
+                    li.setBackgroundColor(Color.LTGRAY);
                 }
                 boolean t = (c.isChecked());
                 if(t==false)
                 {
                     LinearLayout li = row.findViewById(R.id.lo);
+                    Toast.makeText(context,"What are you doing?." , Toast.LENGTH_SHORT).show();
+                    td.get(pos).done = 0;
+                    storeDB(td);
                     li.setBackgroundColor(Color.WHITE);
                 }
 
@@ -155,26 +173,19 @@ public class CustomListAdapter extends ArrayAdapter {
 
             @Override
             public void onClick(View arg0) {
-                int pos = (int)arg0.getTag();
-                td.remove(pos);
-                CustomListAdapter.this.notifyDataSetChanged();            }
+                int posdel = (int)arg0.getTag();
+                Toast.makeText(context, td.get(posdel).head+ " " + posdel+"", Toast.LENGTH_SHORT).show();
+                td.remove(posdel);
+
+                storeDB(td);
+                CustomListAdapter.this.notifyDataSetChanged();
+                CustomListAdapter.this.notifyDataSetInvalidated();
+
+
+
+            }
         });
 
-        Button b1 = (Button) row.findViewById(R.id.confirm);
-        b1.setTag(position);
-        holder.con.setOnClickListener(new View.OnClickListener() {
-            private int pos = position;
-
-            @Override
-            public void onClick(View arg0) {
-                int pos = (int)arg0.getTag();
-
-                String temp = td.get(pos).head ;
-
-                String temp2 = "Task " + td.get(pos).head + " has been created";
-                Toast.makeText(context,temp2 , Toast.LENGTH_SHORT).show();
-                }
-        });
 
         return row;
 
@@ -182,13 +193,49 @@ public class CustomListAdapter extends ArrayAdapter {
 
     }
 
+    public void storeDB(ArrayList<tdItem> t)
+    {
+        SQLiteDatabase DB = this.context.openOrCreateDatabase("tasks.db",0, null);
 
+        DB.execSQL("CREATE TABLE IF NOT EXISTS task (head VARCHAR(200), sub VARCHAR(200), is_done INT)");
+        int i;
+        DB.execSQL("delete from task");
+        for(i=0;i<t.size();++i)
+        {
+            ContentValues row1 = new ContentValues();
+            row1.put("head", t.get(i).head);
+            row1.put("sub", t.get(i).sub);
+            row1.put("is_done", t.get(i).done);
+            DB.insert("task", null, row1);
+        }
+        DB.close();
+
+    }
+    public ArrayList<tdItem> getDB() {
+        ArrayList<tdItem> t = new ArrayList<tdItem>();
+        SQLiteDatabase DB = this.context.openOrCreateDatabase("tasks.db", MODE_PRIVATE, null);
+        Cursor myCursor = DB.rawQuery("select * from task", null);
+        int i = 0;
+        tdItem temp = null;
+        while (myCursor.moveToNext()) {
+            temp = new tdItem();
+            temp.head = myCursor.getString(0);
+            temp.sub = myCursor.getString(1);
+            temp.done = myCursor.getInt(2);
+            t.add(temp);
+            i++;
+        }
+
+        myCursor.close();
+        DB.close();
+        return t;
+    }
 }
  class tdHolder {
     EditText tdH;
     EditText tdS;
     CheckBox tdC;
-    Button con;
+    LinearLayout tdL;
     Button del;
     int pos;
 }
--- app/src/main/java/io/github/shivakanthsujit/procrastinatorplus/MainActivity.java
+++ app/src/main/java/io/github/shivakanthsujit/procrastinatorplus/MainActivity.java
@@ -1,7 +1,10 @@
 package io.github.shivakanthsujit.procrastinatorplus;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -21,71 +24,124 @@ public class MainActivity extends AppCompatActivity {
     ListView list;
     Button add;
     int listpos;
-    ArrayList<tdItem> tdList= new ArrayList<tdItem>();
+    ArrayList<tdItem> tdList = new ArrayList<tdItem>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this,"Press the check mark on the right to save your task." , Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Press the check mark on the right to save your task.", Toast.LENGTH_LONG).show();
         list = findViewById(R.id.list);
         add = findViewById(R.id.add);
+        int k = checkDB();
+        if (k == 0) {
+            storeDB(tdList);
+            Toast.makeText(MainActivity.this, "First Task", Toast.LENGTH_LONG).show();
+        } else {
+            tdList = getDB();
+            Toast.makeText(MainActivity.this, tdList.size()+" tasks created", Toast.LENGTH_LONG).show();
+        }
         final CustomListAdapter whatever = new CustomListAdapter(this, tdList);
         list.setAdapter(whatever);
-        /*@Override
-        public void onItemClick(AdapterView adapterView, View view, int position, long id) {
-
-            long viewId = view.getId();
-
-            if (viewId == R.id.someName) {
-                Toast.makeText(getActivity(), "someName item clicked", Toast.LENGTH_SHORT).show();
-            }
-            else {
-                Toast.makeText(getActivity(), "ListView clicked: " + id, Toast.LENGTH_SHORT).show();
-
-            }
-        }
-*/
 
         add.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
                 tdItem temp = new tdItem();
-                tdList.add(temp);
-                whatever.notifyDataSetChanged();
+                if (checkDB() == 0)
+                {
+                    tdList.add(temp);
+                    whatever.notifyDataSetChanged();
+                    //Toast.makeText(MainActivity.this, "First Task", Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    tdList = getDB();
+                    tdList.add(temp);
+                    whatever.notifyDataSetChanged();
+                    //Toast.makeText(MainActivity.this, tdList.size()+" tasks created", Toast.LENGTH_SHORT).show();
+                }
+
+                storeDB(tdList);
+                final CustomListAdapter whatever = new CustomListAdapter(MainActivity.this, tdList);
+                list.setAdapter(whatever);
             }
         });
 
 
-
-
-
-        /*list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-                tdItem temp = tdList.get(position);
-                listpos = position;
-            }
-        });*/
-
     }
+
     @Override
     public boolean dispatchTouchEvent(MotionEvent event) {
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
             View v = getCurrentFocus();
-            if ( v instanceof EditText) {
+            if (v instanceof EditText) {
                 Rect outRect = new Rect();
                 v.getGlobalVisibleRect(outRect);
-                if (!outRect.contains((int)event.getRawX(), (int)event.getRawY())) {
+                if (!outRect.contains((int) event.getRawX(), (int) event.getRawY())) {
                     v.clearFocus();
                     InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
                     imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
                 }
             }
         }
-        return super.dispatchTouchEvent( event );
+        return super.dispatchTouchEvent(event);
+    }
+
+    public void storeDB(ArrayList<tdItem> t) {
+        SQLiteDatabase DB = openOrCreateDatabase("tasks.db", MODE_PRIVATE, null);
+        DB.execSQL("CREATE TABLE IF NOT EXISTS task (head VARCHAR(200), sub VARCHAR(200), is_done INT)");
+        int i;
+        DB.execSQL("delete from task");
+        for (i = 0; i < t.size(); ++i) {
+            ContentValues row1 = new ContentValues();
+            row1.put("head", t.get(i).head);
+            row1.put("sub", t.get(i).sub);
+            row1.put("is_done", t.get(i).done);
+            DB.insert("task", null, row1);
+        }
+        DB.close();
+    }
+
+    public ArrayList<tdItem> getDB() {
+        ArrayList<tdItem> t = new ArrayList<tdItem>();
+        SQLiteDatabase DB = openOrCreateDatabase("tasks.db", MODE_PRIVATE, null);
+        Cursor myCursor = DB.rawQuery("select * from task", null);
+        int i = 0;
+        tdItem temp = null;
+        while (myCursor.moveToNext()) {
+            temp = new tdItem();
+            temp.head = myCursor.getString(0);
+            temp.sub = myCursor.getString(1);
+            temp.done = myCursor.getInt(2);
+            t.add(temp);
+            i++;
+        }
+
+        myCursor.close();
+        DB.close();
+        return t;
+    }
+
+    public int checkDB() {
+        SQLiteDatabase DB = openOrCreateDatabase("tasks.db", MODE_PRIVATE, null);
+        DB.execSQL("CREATE TABLE IF NOT EXISTS task (head VARCHAR(200), sub VARCHAR(200), is_done INT)");
+
+        Cursor myCursor = DB.rawQuery("select * from task", null);
+        int i = 0;
+
+        while (myCursor.moveToNext()) {
+            i++;
+        }
+
+        myCursor.close();
+        DB.close();
+        return i;
+    }
+
+    public static void tt()
+    {
+
     }
 
 }
-
